1ca0043c24f40429c14bed87190372d2ee3c80be
Asumie-code <darkeangel.1998.ia@gmail.com>
2022-06-03 19:08:36 +0100

    'minor change'

--- routes/posts.js
+++ routes/posts.js
@@ -13,8 +13,8 @@ const imagesPath = `./img/`;
 
 
 router.post('/gallery',  (req, res, next) => {
-    
-   
+
+   console.log(req.query.user)
     const form = formidable({uploadDir: imagesPath, keepExtensions: true});
     form.parse(req , async (err, fields, files) => {
         if(err) {
@@ -50,20 +50,24 @@ router.post('/gallery',  (req, res, next) => {
 
 router.get('/gallery', async (req, res, next) => {
 
-
+   
     
     try {
         let user = await User.findOne({ _id: req.query.id   });
-        for (let index in user.posts) {
-            user.posts[index] = {
-                id: user.posts[index]._id,
-                title: user.posts[index].title,
-                description: user.posts[index].description,
-                img: `http://localhost:5000/assets/images/${user.posts[index].img.slice(6)}`
-            };
-        }
+       if(user) {
+           for (let index in user.posts) {
+               user.posts[index] = {
+                   id: user.posts[index]._id,
+                   title: user.posts[index].title,
+                   description: user.posts[index].description,
+                   img: `http://localhost:5000/assets/images/${user.posts[index].img.slice(6)}`
+               };
+           }
+       } else {
+           return res.status(400).json({error: 'bad request'}); 
+       }
 
-
+       
         res.status(200).json(user.posts);
     } catch (e) {
         next(e)
